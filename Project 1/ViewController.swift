e0fc1074b3977d45dbe94d464f03892b8e50e544
Areeb Durrani <areeb702@revature.net>
2024-12-28 18:50:52 -0800

    Initial Commit

---
+++ Project 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 1
+//
+//  Created by Areeb Durrani on 12/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
