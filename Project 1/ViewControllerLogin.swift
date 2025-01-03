9acce14713e6eb80041216c1b5c876d1a301184d
Areeb Durrani <areeb702@revature.net>
2025-01-02 22:29:19 -0800

    login persists

--- Project 1/ViewControllerLogin.swift
+++ Project 1/ViewControllerLogin.swift
@@ -1,30 +1,29 @@
-//
-//  ViewControllerLogin.swift
-//  Project 1
-//
-//  Created by Areeb Durrani on 12/30/24.
-//
-
 import UIKit
 
 class ViewControllerLogin: UIViewController {
-    var username : String?
-    var password : String?
-    
-    
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var usernameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
+    
     @IBAction func loginPressed(_ sender: Any) {
-            if(usernameTextField.text == username && passwordTextField.text == password){
-                performSegue(withIdentifier: "loginSegue", sender: self)
+        guard let usernameInput = usernameTextField.text,
+              let passwordInput = passwordTextField.text else {
+            print("Username or password field is empty")
+            return
+        }
+        
+        let defaults = UserDefaults.standard
+        let savedUsername = defaults.string(forKey: "registeredUsername")
+        let savedPassword = defaults.string(forKey: "registeredPassword")
+        
+        if usernameInput == savedUsername && passwordInput == savedPassword {
+            print("Login successful")
+            performSegue(withIdentifier: "loginSegue", sender: self)
+        } else {
+            print("Invalid username or password")
         }
     }
-    
-   
 }
-    
-
--- Project 1/ViewControllerRegister.swift
+++ Project 1/ViewControllerRegister.swift
@@ -1,34 +1,25 @@
-//
-//  ViewControllerRegister.swift
-//  Project 1
-//
-//  Created by Areeb Durrani on 12/30/24.
-//
-
 import UIKit
 
 class ViewControllerRegister: UIViewController {
-    var username = ""
-    var password = ""
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
     
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var usernameTextField: UITextField!
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        let dest = segue.destination as! ViewControllerLogin
-        // Pass the selected object to the new view controller.
-        username = usernameTextField.text!
-        password = passwordTextField.text!
-        dest.username = username
-        dest.password = password
+    
+    @IBAction func registerPressed(_ sender: Any) {
+        guard let username = usernameTextField.text, !username.isEmpty,
+              let password = passwordTextField.text, !password.isEmpty else {
+            print("Username or password is empty")
+            return
+        }
+        
+        // Save to UserDefaults
+        let defaults = UserDefaults.standard
+        defaults.set(username, forKey: "registeredUsername")
+        defaults.set(password, forKey: "registeredPassword")
+        
+        print("User registered: \(username)")
     }
-
-
 }
