7b7b6ab1f914fef8658ceac6ff0d082fa3fa078b
Areeb Durrani <areeb702@revature.net>
2025-01-05 13:34:55 -0800

    add music playback

--- Project 1/TableViewControllerMusic.swift
+++ Project 1/TableViewControllerMusic.swift
@@ -1,38 +1,61 @@
-//
-//  TableViewControllerMusic.swift
-//  Project 1
-//
-//  Created by Areeb Durrani on 12/28/24.
-//
-
 import UIKit
+import AVFoundation
 
 class TableViewControllerMusic: UITableViewController {
     
+    var audioPlayer: AVAudioPlayer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    // Data Array for Table View
-    let songs : [[String]] = [["Song 1", "Song 2", "Song 3", "Song 4",  "Song 5"]]
-    
-    //Create sections based on the length of the data array
+    // Nested Array for Songs and Audio File Names
+    let songsAndAudioFiles: [[(title: String, fileName: String)]] = [
+        [
+            ("Moonlight Sonata", "moonlight_sonata")
+        ]
+    ]
+
+    // Create sections based on the length of the nested array
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return songs.count
+        return songsAndAudioFiles.count
     }
     
-    //Create row based on the length of the section subarray
+    // Create rows based on the length of the section subarray
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return songs[section].count
+        return songsAndAudioFiles[section].count
     }
     
-    //Fill cell text based on the data array
+    // Fill cell text based on the nested array
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "musicCell", for: indexPath)
         var contentConfiguration = cell.defaultContentConfiguration()
-        contentConfiguration.text = songs[indexPath.section][indexPath.row]
+        contentConfiguration.text = songsAndAudioFiles[indexPath.section][indexPath.row].title
         contentConfiguration.image = UIImage(named: "music")
         cell.contentConfiguration = contentConfiguration
         return cell
     }
+
+    // Handle cell selection to play the corresponding audio file
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let audioFileName = songsAndAudioFiles[indexPath.section][indexPath.row].fileName
+        playAudio(fileName: audioFileName)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    // Function to play audio using NSDataAsset
+    private func playAudio(fileName: String) {
+        guard let audioDataAsset = NSDataAsset(name: fileName) else {
+            print("Audio asset not found: \(fileName)")
+            return
+        }
+
+        do {
+            audioPlayer = try AVAudioPlayer(data: audioDataAsset.data)
+            audioPlayer?.play()
+        } catch {
+            print("Error playing audio: \(error.localizedDescription)")
+        }
+    }
 }
+
