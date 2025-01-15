6eca61c7ffebc32ec234b9174061c7309955d949
Areeb Durrani <areeb702@revature.net>
2025-01-15 06:49:09 -0800

    refactor books

--- Project 1/TableViewControllerBooks.swift
+++ Project 1/TableViewControllerBooks.swift
@@ -3,18 +3,21 @@ import PDFKit
 
 class CollectionViewControllerBooks: UICollectionViewController {
     
-    let books: [[(title: String, fileName: String)]] = [[
-        ("Armageddon 2419 A.D.", "armageddon"), ("testBook2", "armageddon"), ("testBook3", "armageddon"),
-        ("testBook4", "armageddon"), ("testBook5", "armageddon"), ("testBook6", "armageddon"),
-        ("testBook7", "armageddon"), ("testBook8", "armageddon"), ("testBook9", "armageddon"),
-        ("testBook10", "armageddon"), ("testBook11", "armageddon")
-    ]]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureCollectionViewLayout()
     }
 
+    func getBooks() -> [[(title: String, fileName: String)]] {
+        return [[
+            ("Armageddon 2419 A.D.", "armageddon"), ("testBook2", "armageddon"), ("testBook3", "armageddon"),
+            ("testBook4", "armageddon"), ("testBook5", "armageddon"), ("testBook6", "armageddon"),
+            ("testBook7", "armageddon"), ("testBook8", "armageddon"), ("testBook9", "armageddon"),
+            ("testBook10", "armageddon"), ("testBook11", "armageddon")
+        ]]
+    }
+    
     private func configureCollectionViewLayout() {
         let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize(width: view.bounds.width, height: 50)
@@ -23,17 +26,17 @@ class CollectionViewControllerBooks: UICollectionViewController {
     }
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return books.count
+        return getBooks().count
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return books[section].count
+        return getBooks()[section].count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "booksCell", for: indexPath)
         var contentConfiguration = UIListContentConfiguration.cell()
-        let book = books[indexPath.section][indexPath.row]
+        let book = getBooks()[indexPath.section][indexPath.row]
         contentConfiguration.text = book.title
         contentConfiguration.image = UIImage(named: "book")
         cell.contentConfiguration = contentConfiguration
@@ -43,7 +46,7 @@ class CollectionViewControllerBooks: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
 
-        let selectedBook = books[indexPath.section][indexPath.row]
+        let selectedBook = getBooks()[indexPath.section][indexPath.row]
         let pdfViewController = PDFViewController()
         pdfViewController.pdfFileName = selectedBook.fileName
 
@@ -51,103 +54,20 @@ class CollectionViewControllerBooks: UICollectionViewController {
     }
 }
 
-class CollectionViewControllerRecipes: UICollectionViewController {
+class CollectionViewControllerRecipes: CollectionViewControllerBooks {
     
-    let books: [[(title: String, fileName: String)]] = [[
-        ("Armageddon 2419 A.D.", "armageddon"), ("testBook2", "armageddon"), ("testBook3", "armageddon"),
-        ("testBook4", "armageddon"), ("testBook5", "armageddon"), ("testBook6", "armageddon"),
-        ("testBook7", "armageddon"), ("testBook8", "armageddon"), ("testBook9", "armageddon"),
-        ("testBook10", "armageddon"), ("testBook11", "armageddon")
-    ]]
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        configureCollectionViewLayout()
-    }
-
-    private func configureCollectionViewLayout() {
-        let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: view.bounds.width, height: 50)
-        layout.minimumLineSpacing = 1
-        collectionView.collectionViewLayout = layout
-    }
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return books.count
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return books[section].count
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "booksCell", for: indexPath)
-        var contentConfiguration = UIListContentConfiguration.cell()
-        let book = books[indexPath.section][indexPath.row]
-        contentConfiguration.text = book.title
-        contentConfiguration.image = UIImage(named: "book")
-        cell.contentConfiguration = contentConfiguration
-        return cell
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        collectionView.deselectItem(at: indexPath, animated: true)
-
-        let selectedBook = books[indexPath.section][indexPath.row]
-        let pdfViewController = PDFViewController()
-        pdfViewController.pdfFileName = selectedBook.fileName
-
-        navigationController?.pushViewController(pdfViewController, animated: true)
+    override func getBooks() -> [[(title: String, fileName: String)]] {
+        return [[
+            ("Cheesebsurger", "armageddon"), ("Pizza", "armageddon"), ("Hot Dog", "armageddon"),
+        ]]
     }
 }
 
-class CollectionViewControllerTechnical: UICollectionViewController {
-    
-    let books: [[(title: String, fileName: String)]] = [[
-        ("Armageddon 2419 A.D.", "armageddon"), ("testBook2", "armageddon"), ("testBook3", "armageddon"),
-        ("testBook4", "armageddon"), ("testBook5", "armageddon"), ("testBook6", "armageddon"),
-        ("testBook7", "armageddon"), ("testBook8", "armageddon"), ("testBook9", "armageddon"),
-        ("testBook10", "armageddon"), ("testBook11", "armageddon")
-    ]]
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        configureCollectionViewLayout()
-    }
-
-    private func configureCollectionViewLayout() {
-        let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: view.bounds.width, height: 50)
-        layout.minimumLineSpacing = 1
-        collectionView.collectionViewLayout = layout
-    }
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return books.count
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return books[section].count
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "booksCell", for: indexPath)
-        var contentConfiguration = UIListContentConfiguration.cell()
-        let book = books[indexPath.section][indexPath.row]
-        contentConfiguration.text = book.title
-        contentConfiguration.image = UIImage(named: "book")
-        cell.contentConfiguration = contentConfiguration
-        return cell
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        collectionView.deselectItem(at: indexPath, animated: true)
-
-        let selectedBook = books[indexPath.section][indexPath.row]
-        let pdfViewController = PDFViewController()
-        pdfViewController.pdfFileName = selectedBook.fileName
-
-        navigationController?.pushViewController(pdfViewController, animated: true)
+class CollectionViewControllerTechnical: CollectionViewControllerBooks {
+    override func getBooks() -> [[(title: String, fileName: String)]] {
+        return [[
+            ("C", "armageddon"), ("C++", "armageddon"), ("Java", "armageddon"), ("Swift", "armageddon"),
+        ]]
     }
 }
 
